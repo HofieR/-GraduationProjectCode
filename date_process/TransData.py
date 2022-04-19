6bcfb6135a5b72e55c6ddd2a6181da45f1059df0
Hofie <514220045@qq.com>
2022-04-20 01:24:55 +0800

    update data process

--- date_process/TransData.py
+++ date_process/TransData.py
@@ -1,5 +1,6 @@
 from xlrd import open_workbook
 import os
+import csv
 
 
 def findAllIndex(str_, a):
@@ -15,69 +16,65 @@ def findAllIndex(str_, a):
     return index_list
 
 
-xlsFile = open_workbook(r'../selenium_data/test.xls')
+def splitText(textSet, keywordsSet):
+    textKeywordsSet = []
+    for i in range(len(textSet)):
+        text = textSet[i]
+        keywords = keywordsSet[i]
+        if keywords == "":
+            continue
+        tmpTextSet = text.split("。")
+        for j in range(len(tmpTextSet)):
+            tmpText = tmpTextSet[j]
+            if len(tmpText) > 10:
+                textKeywordsSet.append((tmpText, keywords))
+    return textKeywordsSet
+
+
+def processDate(outFile, textKeywordsSet):
+    for i in range(len(textKeywordsSet)):
+        text = textKeywordsSet[i][0]
+        keywordsSet = textKeywordsSet[i][1]
+        keywords = keywordsSet.split(",")
+        jsonText = "{\"text\": \"" + text + "\", \"label\": {\"keyword\": {"
+        tmp1 = 0
+        for keyword in keywords:
+            if text.find(keyword) > -1:
+                if tmp1 != 0:
+                    jsonText += ", "
+                tmp1 += 1
+                jsonText += "\"" + keyword + "\": ["
+                begIdxes = findAllIndex(text, keyword)
+                keywordLen = len(keyword)
+                tmp2 = 0
+                for begIdx in begIdxes:
+                    if tmp2 != 0:
+                        jsonText += ", "
+                    tmp2 += 1
+                    jsonText += "[" + str(begIdx) + ", " + str(begIdx + keywordLen - 1) + "]"
+                jsonText = jsonText + "]"
+        if tmp1 == 0:
+            jsonText = ""
+        else:
+            jsonText += "}}}\n"
+        outFile.write(jsonText)
+    outFile.close()
+
+
+xlsFile = open_workbook(r'../date_process/data_native/dev.xls')
 sheet = xlsFile.sheet_by_index(0)
 textSet = sheet.col_values(0)
 keywordsSet = sheet.col_values(1)
+textKeywordsSet = splitText(textSet, keywordsSet)
+
 jsonText = ""
-os.remove("train2.json")
-os.remove("dev2.json")
-os.remove("test2.json")
-trainFile = open("train2.json", "a")
-devFile = open("dev2.json", "a")
-testFile = open("test2.json", "a")
+os.remove("../date_process/data_processed/dev.json")
+trainFile = open("../date_process/data_processed/dev.json", "a")
 
-for i in range(8000):
-    text = textSet[i]
-    keywords = keywordsSet[i].split(".")
-    jsonText = "{\"text\": \"" + text + "\", \"label\": {\"keyword\": {"
-    tmp1 = 0
-    for keyword in keywords:
-        if text.find(keyword) > -1:
-            if tmp1 != 0:
-                jsonText += ", "
-            tmp1 += 1
-            jsonText += "\"" + keyword + "\": ["
-            begIdxes = findAllIndex(text, keyword)
-            keywordLen = len(keyword)
-            tmp2 = 0
-            for begIdx in begIdxes:
-                if tmp2 != 0:
-                    jsonText += ", "
-                tmp2 += 1
-                jsonText += "[" + str(begIdx) + ", " + str(begIdx + keywordLen - 1) + "]"
-            jsonText = jsonText + "]"
-    if tmp1 == 0:
-        jsonText = ""
-    else:
-        jsonText += "}}}\n"
-    trainFile.write(jsonText)
+processDate(trainFile, textKeywordsSet)
 
-for i in range(8000, 9000):
-    text = textSet[i]
-    keywords = keywordsSet[i].split(".")
-    jsonText = "{\"text\": \"" + text + "\", \"label\": {\"keyword\": {"
-    tmp1 = 0
-    for keyword in keywords:
-        if text.find(keyword) > -1:
-            if tmp1 != 0:
-                jsonText += ", "
-            tmp1 += 1
-            jsonText += "\"" + keyword + "\": ["
-            begIdxes = findAllIndex(text, keyword)
-            keywordLen = len(keyword)
-            tmp2 = 0
-            for begIdx in begIdxes:
-                if tmp2 != 0:
-                    jsonText += ", "
-                tmp2 += 1
-                jsonText += "[" + str(begIdx) + ", " + str(begIdx + keywordLen - 1) + "]"
-            jsonText = jsonText + "]"
-    if tmp1 == 0:
-        jsonText = ""
-    else:
-        jsonText += "}}}\n"
-    devFile.write(jsonText)
+
+
 
 
 
